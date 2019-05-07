a59b8e33e8228de259eb31315f738f7f62c22949
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2019-05-07 13:59:06 +0900

    大まかにコメント作成

---
+++ src/make.py
@@ -0,0 +1,37 @@
+from PIL import Image, ImageDraw, ImageFont
+
+# コンフィグを書く
+
+# メイン関数でコンフィグの確認(check_config)
+
+# 画像作成関数化
+'''
+色は何色か
+
+どこの回線
+エラー元のIP
+エラー元ホスト名
+
+バツか三角か
+発生時刻
+疎通はできているか
+できていない場合の英語
+
+原因も作成
+'''
+
+def check_config():
+    pass
+
+
+def make_pic():
+    pass
+
+
+def main():
+    check_config()
+    make_pic()
+    
+
+if __name__ == "__main__":
+    main()
