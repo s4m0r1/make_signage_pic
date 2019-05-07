0aefc7be0bdcc3984d737c1bebfcc9e31031aefc
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2019-05-07 15:19:32 +0900

    とりあえずコンフィグまで作成

--- src/make.py
+++ src/make.py
@@ -1,10 +1,25 @@
 from PIL import Image, ImageDraw, ImageFont
+import logging
+from logging import getLogger, StreamHandler, Formatter
 
 # コンフィグを書く
-
-# メイン関数でコンフィグの確認(check_config)
-
+FONT_DIR = ""
+SAVE_SRC = ""
+# LOG = True
+# LOGGER_LEVEL = "DEBUG"
 # 画像作成関数化
+
+# 作成する画像コンフィグ
+COLLOR = ""
+LAN_NAME = ""
+IP_ADDR = ""
+HOST_NAME = ""
+LEVEL = ""
+TIME = ""
+LEVEL_PING = ""
+LEVEL_PING_ENG = ""
+LEVEL_INFO = ""
+
 '''
 色は何色か
 
@@ -21,8 +36,21 @@ from PIL import Image, ImageDraw, ImageFont
 '''
 
 def check_config():
-    pass
+    # if LOG:
+    #     set_logger()
+    #     logging.info("Save Log")
+    if FONT_DIR == "":
+        logging.error("No Set Font! Your Check \"FONT_DIR\"")
+        exit()
+    if SAVE_SRC == "":
+        logging.error("No Set Save Source!Your Check \"SAVE_SRC\"")
+        exit()
 
+
+# def set_logger():
+#     # loggerで受け取るレベル設定
+#     # ハンドラで受け取るレベル設定
+#     pass
 
 def make_pic():
     pass
